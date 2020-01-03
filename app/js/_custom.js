dfbea1ae6c41354987b2ceae22df4e97abe3eb2d
second1996 <vital10012@gmail.com>
2020-01-03 13:51:37 +0200

    Update

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 		nav: true,
 		loop: true,
 		autoplay: true,
-		autoplayTimeout: 5000,
+		autoplayTimeout: 7500,
 		autoplayHoverPause: true,
 		navContainer: '.h-heroes__carousel-nav',
 		navText: [
